42a575bfb3417bfe845efff704a5dc208c9b7258
Jaxon Keeler <jaxon@artifylabs.com>
2018-04-13 19:08:29 -0600

    user info into header after sign in

--- src/nav/UserHeader.vue
+++ src/nav/UserHeader.vue
@@ -1,22 +1,37 @@
 <template>
   <v-toolbar flat class="transparent"> 
     <v-list class="pa-0">
-      <v-list-tile :to="{path: '/settings'}" avatar>
+      <v-list-tile v-if="user" :to="{path: '/settings'}" avatar>
         <v-list-tile-avatar>
-          <img src="https://randomuser.me/api/portraits/men/85.jpg" >
+          <img :src="user.picture">
         </v-list-tile-avatar>
         <v-list-tile-content>
-          <v-list-tile-title>John Leider</v-list-tile-title>
+          <v-list-tile-title>{{user.given_name}} {{user.family_name}}</v-list-tile-title>
         </v-list-tile-content>
           <v-icon class="text-lg-right" >settings</v-icon>
+      </v-list-tile>
+      <v-list-tile v-else :to="{path: '/settings'}" avatar>
+        <v-list-tile-avatar>
+          <v-icon>account_circle</v-icon>
+        </v-list-tile-avatar>
+        <v-list-tile-content>
+          <v-list-tile-title>John Doe</v-list-tile-title>
+        </v-list-tile-content>
+          <v-icon class="text-lg-right" >do_not_disturb_alt</v-icon>
       </v-list-tile>
     </v-list>
   </v-toolbar>
 </template>
 
 <script>
-export default {
+import { mapGetters } from 'vuex'
 
+export default {
+  computed: {
+    ...mapGetters([
+      'user'
+    ])
+  }
 }
 </script>
 
