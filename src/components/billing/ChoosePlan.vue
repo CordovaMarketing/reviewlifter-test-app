2470262d19eccd3738dcf66419328619d89297e6
Tanner Schmoekel <tanner.cordovatech@gmail.com>
2018-05-02 05:52:55 -0600

    Annual vs monthly plans are toggleable

--- src/components/billing/ChoosePlan.vue
+++ src/components/billing/ChoosePlan.vue
@@ -8,10 +8,15 @@
         lg3
       >
         <v-card>
-          <v-card-title><h2>Basic</h2></v-card-title>
-          <v-switch v-model="switch1"></v-switch>
+          <v-card-title><h2>Basic</h2>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-switch class='mt-3' v-model="basicannual" :label='getBasicInfo().label'></v-switch>
+          </v-card-title>
+          
           <v-divider></v-divider>
           <v-list subheader dense>
+            
             <v-card-text>
               <v-list-tile-content><h4>SMS texting</h4></v-list-tile-content>
             </v-card-text>
@@ -32,7 +37,7 @@
             </v-list-tile>
           </v-list>
         <v-list-tile>
-            <v-btn @click="planDialog('basic')" block color="primary" dark>$47</v-btn>
+            <v-btn @click="planDialog('basic')" block color="primary" dark> {{ getBasicInfo().price }} </v-btn>
         </v-list-tile>
         </v-card>
     </v-flex>
@@ -44,7 +49,11 @@
         lg3
       >
         <v-card>
-          <v-card-title><h2>Standard</h2></v-card-title>
+          <v-card-title><h2>Standard</h2>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-switch class='mt-3' v-model="standardannual" :label='getStandardInfo().label'></v-switch>
+          </v-card-title>
 
           <v-divider></v-divider>
           <v-list subheader dense>
@@ -61,7 +70,7 @@
               <v-list-tile-content><h4>500 text limit per month</h4></v-list-tile-content>
             </v-list-tile>
                         <v-list-tile>
-                <v-btn @click="planDialog('standard')" block color="primary" dark>$87</v-btn>
+                <v-btn @click="planDialog('standard')" block color="primary" dark>{{ getStandardInfo().price }} </v-btn>
             </v-list-tile>
           </v-list>
         </v-card>
@@ -74,7 +83,11 @@
         lg3
       >
         <v-card>
-          <v-card-title><h2>Premium</h2></v-card-title>
+          <v-card-title><h2>Premium</h2>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-switch class='mt-3' v-model="premiumannual" :label='getPremiumInfo().label'></v-switch>
+          </v-card-title>
 
           <v-divider></v-divider>
           <v-list subheader dense>
@@ -91,7 +104,7 @@
               <v-list-tile-content><h4>700 Text limit</h4></v-list-tile-content>
             </v-list-tile>
             <v-list-tile>
-                <v-btn @click="planDialog('premium')" block color="primary" dark>$127</v-btn>
+                <v-btn @click="planDialog('premium')" block color="primary" dark> {{ getPremiumInfo().price }} </v-btn>
             </v-list-tile>
           </v-list>
         </v-card>
@@ -104,7 +117,11 @@
         lg3
       >
         <v-card>
-          <v-card-title><h2>Super</h2></v-card-title>
+          <v-card-title><h2>Super</h2>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-switch class='mt-3' v-model="superannual" :label='getSuperInfo().label'></v-switch>
+          </v-card-title>
 
           <v-divider></v-divider>
           <v-list subheader dense>
@@ -121,7 +138,7 @@
               <v-list-tile-content><h4>1000 text limit per month</h4></v-list-tile-content>
             </v-list-tile>
             <v-list-tile>
-                <v-btn @click="planDialog('super')" block color="primary" dark>$197</v-btn>
+                <v-btn @click="planDialog('super')" block color="primary" dark>{{ getSuperInfo().price }}</v-btn>
             </v-list-tile>
             
             
@@ -134,7 +151,7 @@
     <v-dialog v-model="dialog" persistent max-width="500px">
       <v-card>
         <v-card-title>
-          <span class="headline">{{ planName }}</span>
+          <span class="headline">{{ planNameDisplay }}</span>
         </v-card-title>
         <v-divider></v-divider>
         <v-card-text>
@@ -174,7 +191,11 @@ export default {
     data() {
         return{
             dialog: false,
-            switch1: true,
+            basicannual: false,
+            standardannual: false,
+            premiumannual: false,
+            superannual: false,
+            planNameDisplay: '',
             planName: '',
             complete: false,
             stripeOptions: {
@@ -185,12 +206,52 @@ export default {
     methods: {
         planDialog (planName) {
             this.dialog = true
-            this.planName = planName.toUpperCase()
+            if (planName === 'super'){
+              this.planName = this.getSuperInfo().label + planName
+            } 
+            else if (planName === 'premium'){
+              this.planName = this.getPremiumInfo().label + planName
+            }
+            else if (planName === 'standard'){
+              this.planName = this.getStandardInfo().label + planName
+            }
+            else if (planName === 'basic'){
+              this.planName = this.getBasicInfo().label + planName
+            }
+            this.planNameDisplay = planName.toUpperCase()
             this.$store.dispatch('setStripeKey')
         },
         submitPlan () {
-            createToken().then(data => this.$store.dispatch('addPlan', {'plan':this.planName.toLowerCase(), 'stripeToken':data.token.id } ))
+            createToken().then(data => this.$store.dispatch('addPlan', {'plan':this.planName, 'stripeToken':data.token.id } ))
             this.dialog = false
+        },
+        getSuperInfo(){
+          if(this.superannual){
+            return {label: 'Annual', price:'$2130'}
+          }else{
+            return {label: 'Monthly', price:'$127'}
+          }
+        },
+        getPremiumInfo(){
+          if(this.premiumannual){
+            return {label: 'Annual', price:'$1370'}
+          }else{
+            return {label: 'Monthly', price:'$197'}
+          }
+        },
+        getStandardInfo(){
+          if(this.standardannual){
+            return {label: 'Annual', price:'$940'}
+          }else{
+            return {label: 'Monthly', price:'$87'}
+          }
+        },
+        getBasicInfo(){
+          if(this.basicannual){
+            return {label: 'Annual', price:'$505'}
+          }else{
+            return {label: 'Monthly', price:'$47'}
+          }
         }
     },
     components: { Card },
@@ -205,8 +266,15 @@ export default {
 
 <style>
 .stripe-card {
+  position: relative;
+  font-size: 16px;
+  display: inline-block;
+  box-sizing: border-box;
   width: 300px;
-  border: 1px solid grey;
+  max-width: 100%;
+  margin: 0;
+  padding: 15px 0;
+  border-bottom: 1px solid #1976d2;
 }
 .stripe-card.complete {
   border-color: green;
